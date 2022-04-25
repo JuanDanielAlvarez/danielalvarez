81302d0877e140b4faf18c7c4be9a121d4a1f3ce
Juan Álvarez <juan.alvarez02@epn.edu.ec>
2022-04-24 23:54:08 -0500

    [FIX] webkit

--- js/typewritter.js
+++ js/typewritter.js
@@ -1,17 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   
   var speed = 80;
-  
-/*
-  function typeWriter(e, txt, i) {
-    if ((i-1)*2 <= txt.length) { 
-      console.log(e)
-      e.innerHTML = txt.slice(0 , i * 2);
-      i++
-      setTimeout(typeWriter, speed, e, txt, i);
-    }
-  }
-*/
   function typeWriter(element, splitText = [], speed, i) {
     if (i <= splitText.length) {
       var subArray = splitText.slice(0,i);
