f8cfaef631d826e546890886084f27d3e5b9cc48
DanielAlvarez02 <juan.alvarez02@epn.edu.ec>
2022-09-17 12:46:21 -0500

    [ADD] Questions JSON and fixed flow

--- js/typewritter.js
+++ js/typewritter.js
@@ -1,20 +1,29 @@
+
+var stopped = false;
 var speed = 80;
-function typeWriter(element, splitText = [], speed, i) {
-	if (i <= splitText.length) {
-		var subArray = splitText.slice(0, i);
-		element.innerHTML = subArray.join(" ");
-		i++;
-		setTimeout(typeWriter, speed, element, splitText, speed, i);
+function typeWriter(element, splitText = [], speed, currentIndex, twElements, currentElement, callback = (nextElement) => {}) {
+	if (currentIndex <= splitText.length) {
+		var subArray = splitText.slice(0, currentIndex);
+		$(element).html(subArray.join(" "))
+		currentIndex++;
+		if(stopped || !$(element).hasClass("animation-typewritter")){
+			stopped = false;
+			$(element).html(splitText.join(" "))
+		}
+		else{
+			setTimeout(typeWriter, speed, element, splitText, speed, currentIndex, twElements, currentElement, callback);
+		}
+	}else{
+		currentElement++;
+		if(currentElement < $(twElements).length){
+			callback(twElements[currentElement]);
+			typeWriter(twElements[currentElement], $(twElements[currentElement]).html().split(" "), speed, 1, twElements,currentElement, callback);
+		}
 	}
 }
 
-function startTypeWritter() {
-	twElements = document.getElementsByClassName("animation-typewritter");
-	for (var j = 0; j < twElements.length; j++) {
-		typeWriter(twElements[j], twElements[j].innerHTML.split(" "), 120, 0);
-	}
+function startTypeWritter(callback = (nextElement) => {}) {
+	twElements = $(".animation-typewritter").toArray()
+	typeWriter(twElements[0], $(twElements[0]).html().split(" "), 120, 0, twElements,0,callback = (nextElement) => {})
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-	setTimeout(startTypeWritter, 0);
-});
